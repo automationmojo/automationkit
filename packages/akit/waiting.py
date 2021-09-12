8e3829208fd1f83409b2b56b14e299b674f3e557
Myron Walker <myron.walker@gmail.com>
2021-09-12 13:38:16 -0700

    Pass the time parameters to the looper callback for wait_for_it

--- packages/akit/waiting.py
+++ packages/akit/waiting.py
@@ -4,6 +4,8 @@ from typing import Any, Dict, List
 import os
 import time
 
+from datetime import datetime, timedelta
+
 from typing_extensions import Protocol
 
 from akit.exceptions import AKitTimeoutError
@@ -16,7 +18,7 @@ from akit.timeouts import (
 MSG_TEMPL_TIME_COMPONENTS = "    timeout={} start_time={}, end_time={} now_time={} time_diff={}"
 
 class WaitCallback(Protocol):
-    def __call__(self, final_attempt: bool, *args, **kwargs) -> bool:
+    def __call__(self, start: datetime, end: datetime, now:datetime, final_attempt: bool, *args, **kwargs) -> bool:
         """
             This specifies a callable object that can have variable arguments but
             that must have a final_attempt keywork arguement.  The expected behavior
@@ -33,20 +35,21 @@ def wait_for_it(looper: WaitCallback, *, message: str, delay: float=DEFAULT_WAIT
 
     condition_met = False
 
-    start_time = time.time()
+    start_time = datetime.now()
     now_time = start_time
-    end_time = start_time + timeout
+    end_time = start_time + timedelta(seconds=timeout)
     while now_time < end_time:
-        chk_res = looper(False, *looper_args, **looper_kwargs)
+        chk_res = looper(start_time, end_time, now_time, False, *looper_args, **looper_kwargs)
         if chk_res:
             condition_met = True
             break
 
         time.sleep(interval)
-        now_time = time.time()
+        now_time = datetime.now()
 
     if not condition_met:
-        chk_res = looper(True, *looper_args, **looper_kwargs)
+        now_time = datetime.now()
+        chk_res = looper(start_time, end_time, now_time, True, *looper_args, **looper_kwargs)
         if chk_res:
             condition_met = True
 
