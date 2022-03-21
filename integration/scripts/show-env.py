7652de0f118453a06206d24b5b19a8da01ffff3a
Myron Walker <myron.walker@gmail.com>
2022-03-20 21:13:40 -0700

    Change references to REPOSITORY_DIR to AKIT_REPOSITORY_DIR

--- integration/scripts/show-env.py
+++ integration/scripts/show-env.py
@@ -2,6 +2,6 @@ import os
 
 print("=================== Current Environment ===================")
 print("      REPOSITORY_NAME: %s" % os.environ.get("REPOSITORY_NAME", None))
-print(" REPOSITORY_DIRECTORY: %s" % os.environ.get("REPOSITORY_DIRECTORY", None))
+print(" AKIT_REPOSITORY_DIRECTORY: %s" % os.environ.get("AKIT_REPOSITORY_DIRECTORY", None))
 print("       DEVELOPER_MODE: %s" % os.environ.get("DEVELOPER_MODE", None))
 print("           PYTHONPATH: %s" % os.environ.get("PYTHONPATH", None))
