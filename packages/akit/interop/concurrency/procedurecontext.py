36e5fe5f253f46e2c8c52b8a3bd5033df20eb391
Myron Walker <myron.walker@gmail.com>
2022-07-29 12:12:19 -0700

    Fix a bug in ProcedureContext.  It should have a _progress_console member.

--- packages/akit/interop/concurrency/procedurecontext.py
+++ packages/akit/interop/concurrency/procedurecontext.py
@@ -10,11 +10,16 @@ class ProcedureContext(ABC):
 
     def __init__(self, identifier: str):
         self._identifier = identifier
+        self._progress_console = None
         return
 
     @property
     def identifier(self):
         return self._identifier
+
+    @property
+    def progress_console(self):
+        return self._progress_console
 
     @abstractmethod
     def attach_to_progress_console(self, progress_console: Progress):
