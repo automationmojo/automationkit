f04598d9fef6a6f43d51da549b1117c047883b7e
Myron Walker <myron.walker@gmail.com>
2022-12-05 22:39:19 -0600

    Did some refactor work on the automation kit

--- integration/scripts/show-env.py
+++ integration/scripts/show-env.py
@@ -1,7 +1,12 @@
 import os
 
 print("=================== Current Environment ===================")
-print("      REPOSITORY_NAME: %s" % os.environ.get("REPOSITORY_NAME", None))
-print(" AKIT_REPOSITORY_DIRECTORY: %s" % os.environ.get("AKIT_REPOSITORY_DIRECTORY", None))
-print("       DEVELOPER_MODE: %s" % os.environ.get("DEVELOPER_MODE", None))
-print("           PYTHONPATH: %s" % os.environ.get("PYTHONPATH", None))
+
+env_var_names = [ek for ek in os.environ]
+env_var_names.sort()
+
+for ek in env_var_names:
+    print ("{}: {}".format(ek, os.environ[ek]))
+
+print("===========================================================")
+
