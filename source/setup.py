3f8e0a682cee1ae9ac3c2d1fcf9da33129cc3328
Myron Walker <myron.walker@gmail.com>
2022-12-08 12:47:01 -0600

    Fix issue with README in setup.

--- source/setup.py
+++ source/setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import os
+
 from setuptools import setup, find_namespace_packages
 
 AKIT_VERSION = '0.2'
 
-with open("README.md", "r", encoding="utf-8") as fh:
+REPOSITORY_DIR = os.path.dirname(os.path.dirname(__file__))
+README_FILEPATH = os.path.join(REPOSITORY_DIR, "README.md")
+
+with open(README_FILEPATH, "r", encoding="utf-8") as fh:
     LONG_DESCRIPTION = fh.read()
 
 DEPENDENCIES = [
